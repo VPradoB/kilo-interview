60fdba9b1a97dfa2f49b94da8ea2b23954c42a51
Victor Prado <vmpradob@gmail.com>
2025-10-22 20:02:11 -0300

    feat(components): implement mantine for components and clean up react default files

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { MantineProvider } from "@mantine/core";
+
+export default function App() {
+  return (
+    <MantineProvider defaultColorScheme="dark">hello_world</MantineProvider>
+  );
+}
